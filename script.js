36d36df23e5945ffcf64988cd83863190203729b
Gean <000cuiquesuum000@gmail.com>
2020-01-24 02:22:04 +0300

    Timer JS

---
+++ script.js
@@ -0,0 +1,66 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+   
+    //Timer
+
+    let deadline = '2020-02-23';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            // ___________________________________________
+            // if need only hours
+            hours = Math.floor((t/(1000*60*60)));
+            // ___________________________________________
+            //If need days when
+            // hours = Math.floor((t/1000/60/60) % 24),
+            // days = Math.floor((t/(1000*60*60*24)));
+
+            if (t <= 0) {
+                seconds = '0';
+                minutes = '0';
+                hours = '0';
+            }
+            
+
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            
+        for (let key in t) { 
+            if ( t[key] < 10 ) {
+                t[key] = '0' + t[key];      
+            }
+        }    
+
+        
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+
+        }    
+    }
+
+    setClock('timer', deadline);
+});
