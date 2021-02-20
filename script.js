a5cbb526ead59e64dbeb192b2f5e72dc669aaf76
GEAN <000cuiquesuum000@gmail.com>
2021-02-20 11:50:42 +0200

    Added new deadline date

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
-   
     //Timer
 
-    let deadline = '2020-02-23';
+    let deadline = '2021-02-23';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t/1000) % 60),
-            minutes = Math.floor((t/1000/60) % 60),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
             // ___________________________________________
             // if need hours without days 
-            hours = Math.floor((t/(1000*60*60)));
-            // ___________________________________________
-            //If need with days 
-            // hours = Math.floor((t/1000/60/60) % 24),
-            // days = Math.floor((t/(1000*60*60*24)));
+            hours = Math.floor((t / (1000 * 60 * 60)));
+        // ___________________________________________
+        //If need with days 
+        // hours = Math.floor((t/1000/60/60) % 24),
+        // days = Math.floor((t/(1000*60*60*24)));
 
-            if (t <= 0) {
-                seconds = '0';
-                minutes = '0';
-                hours = '0';
-            }
-            
+        if (t <= 0) {
+            seconds = '0';
+            minutes = '0';
+            hours = '0';
+        }
 
-            return {
-                'total': t,
-                'hours': hours,
-                'minutes': minutes,
-                'seconds': seconds
-            };
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
     }
 
     function setClock(id, endtime) {
@@ -38,20 +36,18 @@ window.addEventListener('DOMContentLoaded', function () {
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
-            
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
 
-            
-        for (let key in t) { 
-            if ( t[key] < 10 ) {
-                t[key] = '0' + t[key];      
-            }
-        }    
 
-        
-            hours.textContent = t.hours;
+            for (let key in t) {
+                if (t[key] < 10) {
+                    t[key] = '0' + t[key];
+                }
+            }
+
+            hours.textContent = `${t.hours} h`;
             minutes.textContent = t.minutes;
             seconds.textContent = t.seconds;
 
@@ -59,7 +55,7 @@ window.addEventListener('DOMContentLoaded', function () {
                 clearInterval(timeInterval);
             }
 
-        }    
+        }
     }
 
     setClock('timer', deadline);
